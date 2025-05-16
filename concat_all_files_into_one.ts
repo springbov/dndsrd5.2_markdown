6c489d3c79771d8deeb21443b2e4562a1f775f79
Seth Henderson <springbov@sonofhender.net>
2025-05-15 22:55:02 -0600

    Minor formatting adjustments and adding one markdown to rule them all

--- concat_all_files_into_one.ts
+++ concat_all_files_into_one.ts
@@ -3,14 +3,14 @@
 const src = './src'
 
 function getAllFilePaths(dir: string): Array<string> {
-    const filesAndDirs = Deno.readDirSync(dir);
-    const dirs = filesAndDirs.filter((x) => x.isDirectory);
-    const files = filesAndDirs.filter((x) => x.isFile).map((f) => `${dir}/${f.name}`);
-    const filesFromDirs = dirs.flatMap((dir) => getAllFilePaths(`${dir}/${dir.name}`))
-    return [...files, ...filesFromDirs];
+    const filesAndDirs = [...Deno.readDirSync(dir)]
+    const dirs = filesAndDirs.filter((x) => x.isDirectory)
+    const files = filesAndDirs.filter((x) => x.isFile).map((f) => `${dir}/${f.name}`)
+    const filesFromDirs = dirs.flatMap((subdir) => getAllFilePaths(`${dir}/${subdir.name}`))
+
+    return files.concat(filesFromDirs);
 }
 
 const allFiles = getAllFilePaths(src).sort().map((f) => Deno.readTextFileSync(f));
 
-
-Deno.writeTextFileSync('DND-SRD-5.2-CC.md', allFiles.join(''));
+Deno.writeTextFileSync('DND-SRD-5.2-CC.md', allFiles.join('\n'));
